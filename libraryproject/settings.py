b307c22b62f12dd0d8c87d567ea97a0cf86c4128
VedanshKedia <kediavedansh@gmail.com>
2019-08-09 10:05:20 +0530

    commented the decouple

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from decouple import config
+# from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
